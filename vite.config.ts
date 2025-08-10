0e550e81c8ec155ff4a77bce5e68110ed9a7e533
Almaz Murzabekov <almaz@murzabekov.net>
2025-08-10 22:14:05 +0200

    fix: favicon url

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 export default defineConfig({
   plugins: [react()],
-  base: "./",
+  base: "/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./"),
@@ -13,5 +13,7 @@ export default defineConfig({
   build: {
     target: "esnext",
     outDir: "dist",
+    assetsDir: "assets",
   },
+  publicDir: "public",
 });
